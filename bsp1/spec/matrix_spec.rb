8569d842392786b150fcd6613663210991aadf1c
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-10 15:16:27 -0700

    test: Move tests for directions to princess into robot spec as it feels more like a responsibility of the robot and add tests for creation of a robot object when matrix is initialized

--- bsp1/spec/matrix_spec.rb
+++ bsp1/spec/matrix_spec.rb
@@ -9,16 +9,23 @@ RSpec.describe Matrix do
       expect(matrix).to be_a(Matrix)
     end
 
-    it 'has a size attribute' do
+    it 'has a readable size attribute' do
       matrix = Matrix.new(3, ['---','---','---'])
 
       expect(matrix.size).to eq(3)
     end
 
-    it 'has a grid attribute that maps the rows it is initialized with' do
+    it 'has a readable grid attribute that maps the rows it is initialized with' do
       matrix = Matrix.new(3, ['---','---','---'])
 
       expect(matrix.grid).to eq([['-','-','-'], ['-','-','-'], ['-','-','-']])
+    end
+
+    it 'has a readable robot attribute that is an instance of a robot with the correct coords' do
+      matrix = Matrix.new(3, ['---','-m-','---'])
+
+      expect(matrix.robot).to be_a(Robot)
+      expect(matrix.robot.coords).to eq([1,1])
     end
   end
 
@@ -57,40 +64,6 @@ RSpec.describe Matrix do
           matrix = Matrix.new(5, ['-----','-----','p-m--','-----','-----'])
 
           expect(matrix.get_robot_coords).to eq([2,2])
-        end
-      end
-    end
-
-    describe '#directions_to_princess' do
-      describe 'Happy Path' do
-        it 'returns the correct steps needed to save the princess' do
-          matrix = Matrix.new(3, ['-p-','-m-','---'])
-          robot = Robot.new(matrix.get_robot_coords)
-
-          expect(robot.directions_to_princess).to eq(["UP"])
-        end
-
-        it 'can return the correct steps with the princess in a different location' do
-          matrix = Matrix.new(3, ['--p','-m-','---'])
-          robot = Robot.new(matrix.get_robot_coords)
-
-          expect(robot.directions_to_princess).to eq(["UP", "RIGHT"])
-        end
-
-        it 'can return the correct steps with a different sized grid' do
-          matrix = Matrix.new(5, ['-----','-----','--m--','-----','----p'])
-
-          robot = Robot.new(matrix.get_robot_coords)
-
-          expect(robot.directions_to_princess).to eq(["DOWN", "DOWN", "RIGHT", "RIGHT"])
-        end
-
-        it 'can return the correct steps with a different sized grid going left' do
-          matrix = Matrix.new(5, ['-----','-----','--m--','-----','p----'])
-
-          robot = Robot.new(matrix.get_robot_coords)
-
-          expect(robot.directions_to_princess).to eq(["DOWN", "DOWN", "LEFT", "LEFT"])
         end
       end
     end
