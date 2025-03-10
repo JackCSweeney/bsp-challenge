309581ec74a76a87e05333f647ba968bc4bb2cd4
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-10 14:35:37 -0700

    feat: Add princess_coords attribute and method to find the princess coordinates

--- bsp1/lib/matrix.rb
+++ bsp1/lib/matrix.rb
@@ -4,6 +4,19 @@ class Matrix
   def initialize(size, rows)
     @size = size
     @grid = rows.map {|row| row.chars}
+    @princess_coords = []
   end
 
+  def get_princess_coords
+    for i in (0...@size) do
+      for j in (0...@size) do
+        if @grid[i][j] == 'p'
+          @princess_coords = [j, i]
+          break
+        end
+      end
+    end
+
+    @princess_coords
+  end
 end
