743a93736e43f7fcb1773d25be38fe6ab25b4b07
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-10 13:55:11 -0700

    feat: Add attributes and attr_reader for size and matrix_rows attributes, get_matrix_rows method, validate_row_input(rows) method, and update size methods to set attribute once validated

--- bsp1/lib/input_reader.rb
+++ bsp1/lib/input_reader.rb
@@ -1,12 +1,32 @@
 class InputReader
-  def self.get_matrix_size
-    size = $stdin.gets.to_i
-    validate_size_input(size)
+  attr_reader :size, :matrix_rows
+
+  def initialize
+    @size = 0
+    @matrix_rows = []
   end
 
-  def self.validate_size_input(size)
+  def get_matrix_size
+    size = $stdin.gets.to_i
+    validate_size_input(size)
+    @size = size
+  end
+
+  def validate_size_input(size)
     raise ArgumentError.new("Invalid size: Size must be an odd number between 3 and 100") if size < 3 || size > 100 || size.even?
     
     size
+  end
+
+  def get_matrix_rows
+    rows = $stdin.gets.split
+    validate_row_input(rows)
+    @matrix_rows = rows
+  end
+
+  def validate_row_input(rows)
+    raise ArgumentError.new("Invalid row: Rows do not match the input size") if rows.any?{|row| row.length != @size}
+    raise ArgumentError.new("Invalid row: Number of rows does not match the input size") if rows.length != @size
+    rows
   end
 end
