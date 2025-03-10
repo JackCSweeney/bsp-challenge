f6626d9b652f760fe2a1942156c067e8ec5ab856
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-10 16:13:36 -0700

    feat: Add directions to princess method to Robot class

--- bsp1/lib/robot.rb
+++ bsp1/lib/robot.rb
@@ -3,5 +3,33 @@ class Robot
 
   def initialize(coords)
     @coords = coords
+    @directions = []
+  end
+
+  def directions_to_princess(princess_coords)
+    x_val = @coords[0] - princess_coords[0]
+    y_val = @coords[1] - princess_coords[1]
+
+    if x_val.positive?
+      x_val.times do
+        @directions << "LEFT"
+      end
+    else
+      x_val.abs.times do
+        @directions << "RIGHT"
+      end
+    end
+
+    if y_val.positive?
+      y_val.times do
+        @directions << "UP"
+      end
+    else
+      y_val.abs.times do
+        @directions << "DOWN"
+      end
+    end
+
+    @directions
   end
 end
