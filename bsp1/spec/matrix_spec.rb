36dc9b056cbb00a7cdde84a49a435f0769ed2b48
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-10 14:35:03 -0700

    test: Add tests for finding the x and y coordinates of the princess in the matrix grid

--- bsp1/spec/matrix_spec.rb
+++ bsp1/spec/matrix_spec.rb
@@ -15,10 +15,34 @@ RSpec.describe Matrix do
       expect(matrix.size).to eq(3)
     end
 
-    it 'has a grid attribute that maps the rows it is initialize with' do
+    it 'has a grid attribute that maps the rows it is initialized with' do
       matrix = Matrix.new(3, ['---','---','---'])
 
       expect(matrix.grid).to eq([['-','-','-'], ['-','-','-'], ['-','-','-']])
+    end
+  end
+
+  describe '#instance methods' do
+    describe '.get_princess_coords' do
+      describe 'Happy Path' do
+        it 'will return the coordinates of the princess within the grid' do
+          matrix = Matrix.new(3, ['-p-','---','---'])
+
+          expect(matrix.get_princess_coords).to eq([1,0])
+        end
+
+        it 'can return the coordinates with the princess in a different location' do
+          matrix = Matrix.new(3, ['---','---','p--'])
+
+          expect(matrix.get_princess_coords).to eq([0,2])
+        end
+
+        it 'can return the coordinates with a different size grid' do
+          matrix = Matrix.new(5, ['-----','-----','p----','-----','-----'])
+
+          expect(matrix.get_princess_coords).to eq([0,2])
+        end
+      end
     end
   end
 end
