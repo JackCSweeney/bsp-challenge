0555f0a2753819596455e4f84855a9ed41b8791c
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-10 15:17:18 -0700

    test: Add tests for directions to princess for robot

--- bsp1/spec/robot_spec.rb
+++ bsp1/spec/robot_spec.rb
@@ -14,5 +14,34 @@ RSpec.describe Robot do
 
       expect(robot.coords).to eq([2,2])
     end
+
+    describe '#directions_to_princess' do
+      describe 'Happy Path' do
+        it 'returns the correct steps needed to save the princess' do
+          matrix = Matrix.new(3, ['-p-','-m-','---'])
+          robot = matrix.robot
+
+          expect(robot.directions_to_princess).to eq(["UP"])
+        end
+
+        it 'can return the correct steps with the princess in a different location' do
+          matrix = Matrix.new(3, ['--p','-m-','---'])
+
+          expect(robot.directions_to_princess).to eq(["UP", "RIGHT"])
+        end
+
+        it 'can return the correct steps with a different sized grid' do
+          matrix = Matrix.new(5, ['-----','-----','--m--','-----','----p'])
+
+          expect(robot.directions_to_princess).to eq(["DOWN", "DOWN", "RIGHT", "RIGHT"])
+        end
+
+        it 'can return the correct steps with a different sized grid going left' do
+          matrix = Matrix.new(5, ['-----','-----','--m--','-----','p----'])
+
+          expect(robot.directions_to_princess).to eq(["DOWN", "DOWN", "LEFT", "LEFT"])
+        end
+      end
+    end
   end
 end
