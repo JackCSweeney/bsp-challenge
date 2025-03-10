b13c9a565166471b1611cba3d1bde0541858d61a
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-10 16:13:10 -0700

    test: Reorder expected results for directions

--- bsp1/spec/robot_spec.rb
+++ bsp1/spec/robot_spec.rb
@@ -1,5 +1,6 @@
 require_relative './spec_helper'
 require_relative '../lib/robot.rb'
+require_relative '../lib/matrix.rb'
 
 RSpec.describe Robot do
   describe 'Initialize' do
@@ -19,27 +20,34 @@ RSpec.describe Robot do
       describe 'Happy Path' do
         it 'returns the correct steps needed to save the princess' do
           matrix = Matrix.new(3, ['-p-','-m-','---'])
+          p_coords = matrix.princess_coords
           robot = matrix.robot
 
-          expect(robot.directions_to_princess).to eq(["UP"])
+          expect(robot.directions_to_princess(p_coords)).to eq(["UP"])
         end
 
         it 'can return the correct steps with the princess in a different location' do
           matrix = Matrix.new(3, ['--p','-m-','---'])
+          p_coords = matrix.princess_coords
+          robot = matrix.robot
 
-          expect(robot.directions_to_princess).to eq(["UP", "RIGHT"])
+          expect(robot.directions_to_princess(p_coords)).to eq(["RIGHT", "UP"])
         end
 
         it 'can return the correct steps with a different sized grid' do
           matrix = Matrix.new(5, ['-----','-----','--m--','-----','----p'])
+          p_coords = matrix.princess_coords
+          robot = matrix.robot
 
-          expect(robot.directions_to_princess).to eq(["DOWN", "DOWN", "RIGHT", "RIGHT"])
+          expect(robot.directions_to_princess(p_coords)).to eq(["RIGHT", "RIGHT", "DOWN", "DOWN"])
         end
 
         it 'can return the correct steps with a different sized grid going left' do
           matrix = Matrix.new(5, ['-----','-----','--m--','-----','p----'])
+          p_coords = matrix.princess_coords
+          robot = matrix.robot
 
-          expect(robot.directions_to_princess).to eq(["DOWN", "DOWN", "LEFT", "LEFT"])
+          expect(robot.directions_to_princess(p_coords)).to eq(["LEFT", "LEFT", "DOWN", "DOWN"])
         end
       end
     end
