86662a876a65c020e457e118fc71080ddfd3c231
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-10 14:07:14 -0700

    test: Add tests for matrix attributes

--- bsp1/spec/matrix_spec.rb
+++ bsp1/spec/matrix_spec.rb
@@ -8,5 +8,17 @@ RSpec.describe Matrix do
 
       expect(matrix).to be_a(Matrix)
     end
+
+    it 'has a size attribute' do
+      matrix = Matrix.new(3, ['---','---','---'])
+
+      expect(matrix.size).to eq(3)
+    end
+
+    it 'has a grid attribute that maps the rows it is initialize with' do
+      matrix = Matrix.new(3, ['---','---','---'])
+
+      expect(matrix.grid).to eq([['-','-','-'], ['-','-','-'], ['-','-','-']])
+    end
   end
 end
