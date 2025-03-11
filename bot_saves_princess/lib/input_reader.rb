315c31101b163df5ca8b24983057c0191ab94fc3
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-11 10:49:05 -0700

    feat: Add get_robot_coordinates and @robot_coordinates to input_reader to first check if the first char of the input matches to the digit regex, then set the robot coordinates if it does

--- bot_saves_princess/lib/input_reader.rb
+++ bot_saves_princess/lib/input_reader.rb
@@ -1,9 +1,10 @@
 class InputReader
-  attr_reader :size, :matrix_rows
+  attr_reader :size, :matrix_rows, :robot_coordinates
 
   def initialize
     @size = 0
     @matrix_rows = []
+    @robot_coordinates = []
   end
 
   def get_matrix_size
@@ -25,5 +26,11 @@ class InputReader
     raise ArgumentError.new("Invalid row: Size of rows do not match the input size") if rows.any?{|row| row.length != @size}
     raise ArgumentError.new("Invalid row: Number of rows does not match the input size") if rows.length != @size
     rows
+  end
+
+  def get_robot_coordinates
+    if /[[:digit:]]/.match($stdin.gets[0])
+      @robot_coordinates = $stdin.gets.split.map(&:to_i).reverse
+    end
   end
 end
