ec3ff2ccfd1252e46b0fbfbec0441e0adcb0d198
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-11 14:27:53 -0700

    feat: remove unneeded calls of instance variables when attr_reader is available

--- bot_saves_princess/lib/princess_saver.rb
+++ bot_saves_princess/lib/princess_saver.rb
@@ -2,25 +2,25 @@ require_relative 'input_reader.rb'
 require_relative 'matrix.rb'
 
 class PrincessSaver
-  attr_reader :input_reader
+  attr_reader :input_reader, :matrix
   def initialize
     @input_reader = InputReader.new
     @matrix = nil
   end
 
   def get_input
-    @input_reader.get_matrix_size
-    @input_reader.get_robot_coordinates
-    @input_reader.get_matrix_rows
+    input_reader.get_matrix_size
+    input_reader.get_robot_coordinates
+    input_reader.get_matrix_rows
   end
 
   def make_matrix
-    @matrix = Matrix.new(@input_reader.size, @input_reader.matrix_rows)
+    @matrix = Matrix.new(input_reader.size, input_reader.matrix_rows, input_reader.robot_coordinates)
   end
 
   def save_princess
     get_input
     make_matrix
-    puts @matrix.robot.directions_to_princess(@matrix.princess.coords)
+    puts matrix.robot.directions_to_princess(matrix.princess.coords)
   end
 end
