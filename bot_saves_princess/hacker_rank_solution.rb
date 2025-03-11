d7282c6f396caf46c012a6d07e7aeb23b3816daf
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-11 16:18:24 -0700

    feat: Update hacker_rank_solution file with new changes to make challenge 2 pass in hacker rank

--- bot_saves_princess/hacker_rank_solution.rb
+++ bot_saves_princess/hacker_rank_solution.rb
@@ -1,14 +1,15 @@
 class InputReader
-  attr_reader :size, :matrix_rows
+  attr_reader :size, :matrix_rows, :robot_coordinates
 
   def initialize
     @size = 0
     @matrix_rows = []
+    @robot_coordinates = []
   end
 
   def get_matrix_size
-    size = $stdin.gets.to_i
-    @size = validate_size_input(size)
+    non_validated_size = $stdin.gets.to_i
+    @size = validate_size_input(non_validated_size)
   end
 
   def validate_size_input(size)
@@ -18,55 +19,84 @@ class InputReader
 
   def get_matrix_rows
     rows = []
-    @size.times do
-      rows << $stdin.gets.chomp
-    end
+    @size.times {rows << $stdin.gets.chomp}
     @matrix_rows = validate_row_input(rows)
   end
 
   def validate_row_input(rows)
-    raise ArgumentError.new("Invalid row: Rows do not match the input size") if rows.any?{|row| row.length != @size}
+    raise ArgumentError.new("Invalid row: Size of rows do not match the input size") if rows.any?{|row| row.length != @size}
     raise ArgumentError.new("Invalid row: Number of rows does not match the input size") if rows.length != @size
     rows
   end
+
+  def get_robot_coordinates
+    input = $stdin.gets
+    if /[[:digit:]]/.match(input)
+      coords = input.split.map(&:to_i).reverse
+      @robot_coordinates = validate_robot_coordinates(coords)
+    end
+  end
+
+  def validate_robot_coordinates(coords)
+    raise ArgumentError.new("Invalid location: Robot coordinates must fit with the size of the grid using matrix convention") if coords.any?{|num| num >= @size}
+    coords
+  end
 end
 
-class Robot
-  attr_reader :coords, :directions
+class Matrix
+  attr_reader :size, :grid, :robot, :princess
 
-  def initialize(coords)
-    @coords = coords
-    @directions = []
+  def initialize(size, rows, robot_coords=[])
+    @size = size
+    @grid = rows.map {|row| row.chars}
+    @robot_coords = robot_coords
+    @robot = Robot.new(set_coords_for_robot)
+    @princess = Princess.new(get_princess_coords)
   end
 
-  def directions_to_princess(princess_coords)
-    x_directions(@coords[0] - princess_coords[0])
-    y_directions(@coords[1] - princess_coords[1])
-    @directions
-  end
-
-  def x_directions(x_val)
-    if x_val.positive?
-      x_val.times do
-        @directions << "LEFT"
-      end
-    else
-      x_val.abs.times do
-        @directions << "RIGHT"
-      end
+  def get_princess_coords
+    grid.flatten.each_with_index do |cell, index|
+      return [index % @size, (index / @size)] if cell == 'p'
     end
   end
 
-  def y_directions(y_val)
-    if y_val.positive?
-      y_val.times do
-        @directions << "UP"
-      end
+  def set_coords_for_robot
+    if @robot_coords.empty?
+      [(@size / 2), (@size / 2)]
     else
-      y_val.abs.times do
-        @directions << "DOWN"
-      end
+      @robot_coords
     end
+  end
+end
+
+class PrincessSaver
+  attr_reader :input_reader, :matrix
+  def initialize
+    @input_reader = InputReader.new
+    @matrix = nil
+  end
+
+  def get_input
+    input_reader.get_matrix_size
+    input_reader.get_robot_coordinates
+    input_reader.get_matrix_rows
+  end
+
+  def make_matrix
+    @matrix = Matrix.new(input_reader.size, input_reader.matrix_rows, input_reader.robot_coordinates)
+  end
+
+  def save_princess
+    get_input
+    make_matrix
+    puts matrix.robot.directions_to_princess(matrix.princess.coords)
+  end
+
+  def next_move
+    get_input
+    make_matrix
+    matrix.robot.directions_to_princess(matrix.princess.coords)
+    puts matrix.robot.next_move
   end
 end
 
@@ -78,56 +108,47 @@ class Princess
   end
 end
 
-class Matrix
-  attr_reader :size, :grid, :robot, :princess
+class Robot < Princess
+  attr_reader :directions
 
-  def initialize(size, rows)
-    @size = size
-    @grid = rows.map {|row| row.chars}
-    @robot = Robot.new(get_robot_coords)
-    @princess = Princess.new(get_princess_coords)
+  def initialize(coords)
+    super
+    @directions = []
   end
 
-  def get_princess_coords
-    if @grid[0][0] == 'p'
-      [0,0]
-    elsif @grid[0][@size - 1] == 'p'
-      [@size - 1, 0]
-    elsif @grid[@size - 1][0] == 'p'
-      [0,@size - 1]
-    else
-      [@size - 1, @size - 1]
+  def directions_to_princess(princess_coords)
+    x_directions(@coords[0] - princess_coords[0])
+    y_directions(@coords[1] - princess_coords[1])
+    @directions
+  end
+
+  def x_directions(x_val)
+    return x_val.times {@directions << "LEFT"} if x_val.positive?
+    x_val.abs.times {@directions << "RIGHT"} 
+  end
+
+  def y_directions(y_val)
+    return y_val.times {@directions << "UP"} if y_val.positive?
+    y_val.abs.times {@directions << "DOWN"}  
+  end
+
+  def next_move
+    change_coordinates(@directions.first)
+    @directions.shift
+  end
+
+  def change_coordinates(direction_moved)
+    case direction_moved
+    when "LEFT"
+      @coords[0] -= 1
+    when "RIGHT"
+      @coords[0] += 1
+    when "UP"
+      @coords[-1] += 1
+    when "DOWN"
+      @coords[-1] -= 1
     end
   end
-
-  def get_robot_coords
-    center = @size / 2
-    [center, center]
-  end
 end
 
-class PrincessSaver
-  attr_reader :input_reader
-  def initialize
-    @input_reader = InputReader.new
-    @matrix = nil
-  end
-
-  def get_input
-    @input_reader.get_matrix_size
-    @input_reader.get_matrix_rows
-  end
-
-  def make_matrix
-    @matrix = Matrix.new(@input_reader.size, @input_reader.matrix_rows)
-  end
-
-  def save_princess
-    get_input
-    make_matrix
-    puts @matrix.robot.directions_to_princess(@matrix.princess.coords)
-  end
-end
-
-saver = PrincessSaver.new
-saver.save_princess
+PrincessSaver.new.next_move
