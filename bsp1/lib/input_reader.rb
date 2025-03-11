6ff680d451646696a6dd76d56a3ab0524319922e
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-11 08:47:28 -0700

    feat: Update input reader to set attributes with return value of validations to trim extra lines of code. Tests all still pasing

--- bsp1/lib/input_reader.rb
+++ bsp1/lib/input_reader.rb
@@ -8,24 +8,21 @@ class InputReader
 
   def get_matrix_size
     size = $stdin.gets[0].to_i
-    validate_size_input(size)
-    @size = size
+    @size = validate_size_input(size)
   end
 
   def validate_size_input(size)
     raise ArgumentError.new("Invalid size: Size must be an odd number between 3 and 100") if size < 3 || size > 100 || size.even?
-    
     size
   end
 
   def get_matrix_rows
     rows = $stdin.gets.split[1..-1]
-    validate_row_input(rows)
-    @matrix_rows = rows
+    @matrix_rows = validate_row_input(rows)
   end
 
   def validate_row_input(rows)
-    raise ArgumentError.new("Invalid row: Rows do not match the input size") if rows.any?{|row| row.length != @size}
+    raise ArgumentError.new("Invalid row: Size of rows do not match the input size") if rows.any?{|row| row.length != @size}
     raise ArgumentError.new("Invalid row: Number of rows does not match the input size") if rows.length != @size
     rows
   end
