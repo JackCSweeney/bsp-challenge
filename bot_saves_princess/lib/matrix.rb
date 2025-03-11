3f25a7c5ffa8f5cf75e6eba151820f579b3ca5f7
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-11 11:20:26 -0700

    feat: Add robot_coords argument to initialize with default value of empty array. Update method of get_robot_coords to set_coords_for_robot, now checking if attribute is empty, if yes sets to center, if no, takes the coords given on instantiation

--- bot_saves_princess/lib/matrix.rb
+++ bot_saves_princess/lib/matrix.rb
@@ -4,10 +4,11 @@ require_relative 'princess.rb'
 class Matrix
   attr_reader :size, :grid, :robot, :princess
 
-  def initialize(size, rows)
+  def initialize(size, rows, robot_coords=[])
     @size = size
     @grid = rows.map {|row| row.chars}
-    @robot = Robot.new(get_robot_coords)
+    @robot_coords = robot_coords
+    @robot = Robot.new(set_coords_for_robot)
     @princess = Princess.new(get_princess_coords)
   end
 
@@ -23,8 +24,11 @@ class Matrix
     end
   end
 
-  def get_robot_coords
-    center = @size / 2
-    [center, center]
+  def set_coords_for_robot
+    if @robot_coords.empty?
+      [(@size / 2), (@size / 2)]
+    else
+      @robot_coords
+    end
   end
 end
