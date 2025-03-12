330879d2cfe5847d07d9c72d5f76cd734a60d4d5
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-11 20:02:13 -0700

    refactor: Update tests for princess saver to better isolate individual methods of the class. Test for return values rather than outputs so output can be handled in runner file

--- bot_saves_princess/spec/princess_saver_spec.rb
+++ bot_saves_princess/spec/princess_saver_spec.rb
@@ -15,62 +15,93 @@ RSpec.describe PrincessSaver do
   end
 
   describe '#instance methods' do
-    describe '#save_princess' do
+    describe '#directions_to_princess' do
       describe 'different board sizes and all 4 directions' do
         it 'can find the right directions for a 3x3 board' do
-          # additional empty string added to account for not being able to force stubs to be used a second time during check for robot coordinates and provide sequential input values at the same time to :gets
-          allow($stdin).to receive(:gets).and_return("3","","p--","-m-","---")
-          expected = "LEFT\nUP\n"
+          allow($stdin).to receive(:gets).and_return("3")
+          @saver.input_reader.get_matrix_size
+          allow($stdin).to receive(:gets).and_return("p--","-m-","---")
+          @saver.input_reader.get_matrix_rows
 
-          expect {@saver.save_princess}.to output(expected).to_stdout
+          @saver.make_matrix
+
+          expect(@saver.directions_to_princess).to eq(["LEFT", "UP"])
         end
 
         it 'can find the right directions for a 7x7 board' do
-          allow($stdin).to receive(:gets).and_return("7","","-------","-------","-------","---m---","-------","-------","p------")
-          expected = "LEFT\nLEFT\nLEFT\nDOWN\nDOWN\nDOWN\n"
+          allow($stdin).to receive(:gets).and_return("7")
+          @saver.input_reader.get_matrix_size
+          allow($stdin).to receive(:gets).and_return("-------","-------","-------","---m---","-------","-------","p------")
+          @saver.input_reader.get_matrix_rows
 
-          expect {@saver.save_princess}.to output(expected).to_stdout
+          @saver.make_matrix
+
+          expect(@saver.directions_to_princess).to eq(["LEFT", "LEFT", "LEFT", "DOWN", "DOWN", "DOWN"])
         end
 
         it 'can find the right directions for a 9x9 board' do
-          allow($stdin).to receive(:gets).and_return("9","","---------","---------","---------","---------","----m----","---------","---------","---------","--------p")
-          expected = "RIGHT\nRIGHT\nRIGHT\nRIGHT\nDOWN\nDOWN\nDOWN\nDOWN\n"
+          allow($stdin).to receive(:gets).and_return("9")
+          @saver.input_reader.get_matrix_size
+          allow($stdin).to receive(:gets).and_return("---------","---------","---------","---------","----m----","---------","---------","---------","--------p")
+          @saver.input_reader.get_matrix_rows
 
-          expect {@saver.save_princess}.to output(expected).to_stdout
+          @saver.make_matrix
+
+          expect(@saver.directions_to_princess).to eq(["RIGHT", "RIGHT", "RIGHT", "RIGHT", "DOWN", "DOWN", "DOWN", "DOWN"])
         end
 
         it 'can find the right directions for a 5x5 board' do
-          allow($stdin).to receive(:gets).and_return("5","","----p","-----","--m--","-----","-----")
-          expected = "RIGHT\nRIGHT\nUP\nUP\n"
+          allow($stdin).to receive(:gets).and_return("5")
+          @saver.input_reader.get_matrix_size
+          allow($stdin).to receive(:gets).and_return("----p","-----","--m--","-----","-----")
+          @saver.input_reader.get_matrix_rows
+          
+          @saver.make_matrix
 
-          expect {@saver.save_princess}.to output(expected).to_stdout
+          expect(@saver.directions_to_princess).to eq(["RIGHT", "RIGHT", "UP", "UP"])
         end
       end
     end
 
-    describe '#next_move' do
+    describe '#next_move_to_save_princess' do
       it 'will output only the next move the robot makes towards the princess on a 3x3 board' do
         allow($stdin).to receive(:gets).and_return("3","1 1","p--","-m-","---")
+        @saver.input_reader.get_matrix_size
+        @saver.input_reader.get_robot_coordinates
+        @saver.input_reader.get_matrix_rows
+        @saver.make_matrix
         
-        expect {@saver.next_move}.to output("LEFT\n").to_stdout
+        expect(@saver.next_move_to_save_princess).to eq("LEFT")
       end
 
       it 'will output only the next move the robot makes towards the princess on a 5x5 board' do
         allow($stdin).to receive(:gets).and_return("5","2 2","----p","-----","--m--","-----","-----")
+        @saver.input_reader.get_matrix_size
+        @saver.input_reader.get_robot_coordinates
+        @saver.input_reader.get_matrix_rows
+        @saver.make_matrix
         
-        expect {@saver.next_move}.to output("RIGHT\n").to_stdout
+        expect(@saver.next_move_to_save_princess).to eq("RIGHT")
       end
 
       it 'will output only the next move the robot makes towards the princess on a 3x3 board with princess and robot in different position' do
         allow($stdin).to receive(:gets).and_return("3","2 1","-p-","---","-m-")
+        @saver.input_reader.get_matrix_size
+        @saver.input_reader.get_robot_coordinates
+        @saver.input_reader.get_matrix_rows
+        @saver.make_matrix
         
-        expect {@saver.next_move}.to output("UP\n").to_stdout
+        expect(@saver.next_move_to_save_princess).to eq("UP")
       end
 
       it 'will output only the next move the robot makes towards the princess on a 5x5 board with princess and robot in different position' do
         allow($stdin).to receive(:gets).and_return("5","3 2","-----","-----","-----","--n--","--p--")
+        @saver.input_reader.get_matrix_size
+        @saver.input_reader.get_robot_coordinates
+        @saver.input_reader.get_matrix_rows
+        @saver.make_matrix
 
-        expect {@saver.next_move}.to output("DOWN\n").to_stdout
+        expect(@saver.next_move_to_save_princess).to eq("DOWN")
       end
     end
   end
