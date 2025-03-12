79643e5abe05f69fe22a33825c96139eb7eb8837
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-11 18:32:34 -0700

    refactor: Rename x and y axis direction methods for more clear name of method and variables. Rename move robot method to move robot on x and y axis and update variable names  to be more clear. Simplify logic for moving robot to utilize less conditionals/case statements to condense methods

--- bot_saves_princess/lib/robot.rb
+++ bot_saves_princess/lib/robot.rb
@@ -8,36 +8,34 @@ class Robot < Princess
   end
 
   def directions_to_princess(princess_coordinates)
-    x_axis_moves(@coordinates[:x] - princess_coordinates[:x])
-    y_axis_moves(@coordinates[:y] - princess_coordinates[:y])
+    x_axis_directions(@coordinates[:x] - princess_coordinates[:x])
+    y_axis_directions(@coordinates[:y] - princess_coordinates[:y])
     @directions
   end
 
-  def x_axis_moves(x_axis_distance)
+  def x_axis_directions(x_axis_distance)
     return x_axis_distance.times {@directions << "LEFT"} if x_axis_distance.positive?
     x_axis_distance.abs.times {@directions << "RIGHT"} 
   end
 
-  def y_axis_moves(y_axis_distance)
+  def y_axis_directions(y_axis_distance)
     return y_axis_distance.times {@directions << "UP"} if y_axis_distance.positive?
     y_axis_distance.abs.times {@directions << "DOWN"}  
   end
 
   def next_move
-    change_coordinates(@directions.first)
-    @directions.shift
+    next_direction = @directions.shift
+    return move_robot_on_x_axis(next_direction) if next_direction == "LEFT" || next_direction == "RIGHT"
+    move_robot_on_y_axis(next_direction) if next_direction == "UP" || next_direction == "DOWN"
   end
 
-  def change_coordinates(direction_moved)
-    case direction_moved
-    when "LEFT"
-      @coords[0] -= 1
-    when "RIGHT"
-      @coords[0] += 1
-    when "UP"
-      @coords[-1] += 1
-    when "DOWN"
-      @coords[-1] -= 1
-    end
+  def move_robot_on_x_axis(next_direction)
+    next_direction == "RIGHT" ? @coordinates[:x] += 1 : @coordinates[:x] -= 1
+    next_direction
+  end
+
+  def move_robot_on_y_axis(next_direction)
+    next_direction == "UP" ? @coordinates[:y] += 1 : @coordinates[:y] -= 1
+    next_direction
   end
 end
