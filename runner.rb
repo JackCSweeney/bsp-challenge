ed8b97cb5007550fe86a9a6055ce338c8103b13a
Jack Sweeney <jackcsweeney@gmail.com>
2025-03-12 14:48:38 -0700

    update runner statements for clarity

--- runner.rb
+++ runner.rb
@@ -9,18 +9,19 @@ require './lib/robot.rb'
 puts "Enter which challenge mode you would like to see: 1 to print all directions or 2 to print just the next move"
 mode = $stdin.gets.chomp.to_i
 saver = PrincessSaver.new
-statement = "Please enter an odd number between 3 - 100 and a grid matching that size in length and width.\nUse '-' to denote cells on the grid.\nPlace the robot 'm' in the center and place the princess 'p' in one of the four corners."
+statement_1 = "Please enter an odd number between 3 - 100 and a grid matching that size in length and width.\nUse '-' to denote cells on the grid.\nPlace the robot 'm' in the center and place the princess 'p' in one of the four corners."
+statement_2 = "Please enter an odd number between 3 - 100, the row and column you'd like to place the robot with a blank space between the two numbers, and a grid matching that size in length and width.\nUse '-' to denote cells on the grid.\nUse 'm' to denote the robot and 'p' to denote the princess in the grid.\nThey may be placed wherever you choose"
 
 
 case mode
 when 1
-  puts statement
+  puts statement_1
   saver.input_reader.get_matrix_size
   saver.input_reader.get_matrix_rows
   saver.make_matrix
   puts saver.directions_to_princess
 when 2
-  puts statement
+  puts statement_2
   saver = PrincessSaver.new
   saver.input_reader.get_matrix_size
   saver.input_reader.get_robot_coordinates
